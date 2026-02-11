e27df56fd22954ffe171c38118811470e716067f
consoleolog <leopportunity5@gmail.com>
2026-02-11 12:40:33 +0900

    실행 root 경로의 main 에 기본 스키마 정의
    - main.py: 스키마 정의
    - database.yaml: db 설정 파일
    - exchanges.yaml: 거래 모듈 설정 파일
    - trading_bot.yaml: 애플리케이션 설정 파일

--- src/main.py
+++ src/main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+import asyncio
+import logging
+import os
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dotenv import load_dotenv
+
+sys.path.insert(0, os.getcwd())
+
+from src.config import ConfigManager, ConfigSchema
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+async def main():
+    schema = ConfigSchema()
+    schema.require("trading_bot.mode", str)
+    schema.require("exchanges.executor_name", str)
+
+    config = ConfigManager(schema, watch_interval=2.0)
+
+    config.load_file("config/trading_bot.yaml", required=True)
+    config.load_file("config/database.yaml", required=True)
+
+    if config.get("exchanges.executor_name") == "upbit":
+        executor_config = {
+            "UPBIT_API_KEY": "upbit.access_key",
+            "UPBIT_API_SECRET": "upbit.secret_key",
+        }
+    else:
+        executor_config = {}
+
+    config.load_env(
+        mapping={
+            **executor_config,
+            # Timescale DB
+            "DB_HOST": "timescaledb.host",
+            "DB_PORT": "timescaledb.port",
+            "DB_NAME": "timescaledb.database",
+            "DB_USER": "timescaledb.user",
+            "DB_PASSWORD": "timescaledb.password",
+        }
+    )
+
+    try:
+        config.validate()
+        logger.info("Configuration validated successfully")
+    except Exception as exception:
+        logger.error(f"Configuration validation failed: {exception}")
+        raise
+
+    # TODO: run application
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
+    # Configure logging
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s | %(levelname)8s | %(name)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Run
+    asyncio.run(main())

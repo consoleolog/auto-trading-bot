aece6df37ebfe16783d42b567d7f5b6e124218cc
consoleolog <leopportunity5@gmail.com>
2026-02-11 09:32:37 +0900

    손실을 제한하기 위한 RiskLimitsConfig dataclass 추가
    - risk_limits_config.py: 손실을 제한하기 위한 정보를 담은 dataclass 추가

--- src/risk/models/__init__.py
+++ src/risk/models/__init__.py
@@ -1,5 +1,6 @@
 from .risk_context import RiskContext
+from .risk_limits_config import RiskLimitsConfig
 from .risk_record import RiskRecord
 from .triggered_rule import TriggeredRule
 
-__all__ = ["RiskRecord", "RiskContext", "TriggeredRule"]
+__all__ = ["RiskRecord", "RiskLimitsConfig", "RiskContext", "TriggeredRule"]
